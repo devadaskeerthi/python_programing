0a3c357e5809754e69583caca127ab8e22aa12e5
raaahul3 <rahular7306@gmail.com>
2024-04-30 21:17:46 +0530

    Changed

--- new.py
+++ new.py
@@ -3,12 +3,12 @@ length=len(l)
 new_l=[]
 i=0
 k=3
-sum=0
+sum=[]
 while i<length:
     for j in range(0,k):
         new_l.append(l[i])
         i+=1
         
-    sum+=max(new_l)
+    sum.append(max(new_l))
     new_l.clear()
 print(sum)
