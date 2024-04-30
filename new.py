39b3316c5929d9689e7375e729051b8d1ed10978
raaahul3 <rahular7306@gmail.com>
2024-04-30 22:27:24 +0530

    my code

--- new.py
+++ new.py
@@ -2,15 +2,17 @@ l = list(map(int, input("Enter a list of numbers separated by spaces: ").split()
 
 length=len(l)
 new_l=[]
-i=1
+i=0
 k=int(input("Enter partition limit: "))
 sum=[]
-new_l.append(l[0])
+
 while i<length:
-    for j in range(1,k):
+    for j in range(0,k):
         new_l.append(l[i])
         i+=1
+        
     print(new_l,"-->",max(new_l))
-    b=new_l[k-1]
     new_l.clear()
-    new_l.append(b)
+    if i==length:
+        break
+    i-=2
